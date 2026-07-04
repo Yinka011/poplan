4f20f0ff89e423b05362ce86272ca685390b8315
Yinka011 <olaniyanyin@gmail.com>
2026-07-04 08:01:53 -0700

    Use environment variables for Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://framesmhcepkdheoclsl.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyYW1lc21oY2Vwa2RoZW9jbHNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMDM4NDMsImV4cCI6MjA5ODY3OTg0M30.IlvDNTE-5ueUmfJde89oXCEbVsvGjlo5LeWI-q2kOto'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
